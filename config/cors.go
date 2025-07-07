8f3244925886b362ee2f4781cc0617f0a9b3ef2e
Zahra Dia Handoko <diahandokozahra29@gmail.com>
2025-07-07 14:43:54 +0700

    edit

--- config/cors.go
+++ config/cors.go
@@ -1,11 +1,13 @@
 package config
 
 var AllowedOrigins = []string{
-	"http://localhost:3000",
-	"http://localhost:5173",
-	"http://localhost:6969",
+	"http://localhost:5000",
+	"http://localhost:8080",
+	"https://indrariksa.github.io",
+	"https://frontend-coba-dudm6gdfx-zahradh28s-projects.vercel.app/",
 }
 
 func GetAllowedOrigins() []string {
 	return AllowedOrigins
 }
+
